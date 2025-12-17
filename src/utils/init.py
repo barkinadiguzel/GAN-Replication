d2d01f139aa55cee1d56a547220bcbc0e38a6824
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-17 11:12:52 +0300

    Create init.py

---
+++ src/utils/init.py
@@ -0,0 +1,7 @@
+import torch.nn as nn
+
+def init_weights(module):
+    if isinstance(module, nn.Linear):
+        nn.init.xavier_uniform_(module.weight)
+        if module.bias is not None:
+            nn.init.zeros_(module.bias)
